6294d7f392b18cf51451c6fa8eb153e0281eaca7
nayeemislamnishad <159826869+nayeemislamnishad@users.noreply.github.com>
2024-07-10 00:46:30 +0600

    Create answerSheet.js

---
+++ answerSheet.js
@@ -0,0 +1,6 @@
+const gucco1="bdadcdabcccbaaaabcababaaadaabccacacddabadcdaddabbcdc";
+const gucco2="accabaaaadaccabdaabdbcababcadcdbaba";
+const gucco3="cabaadbadbaccbdaadbbbcabddaca";
+const gucco4="adadabaaabbbcabbacdacadbadbdca";
+const gucco5="abdaaccbabccdbaaabbbdbdcccccbb";
+const gucco6="cbcdbcbaacabbadcdbddccbaababaccbdbbbabca";
